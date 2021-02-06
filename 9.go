61bc5d8cbc8df59ab6b072c6a7bc1416bec44332
“rahulitibd” <rahulitibd@gmail.com>
2021-02-06 16:08:13 +0600

    Add to Bool rune

---
+++ 9.go
@@ -0,0 +1,23 @@
+package main
+import "fmt"
+func main(){
+/*var name string 
+var age int
+var result float32
+var chr rune
+var isFound bool
+fmt.Println(name)
+fmt.Println(age)
+fmt.Println(chr)
+fmt.Println(result)
+fmt.Println(isFound)
+fmt.Println(true && true)
+fmt.Println(true && false)
+fmt.Println(true || true)
+fmt.Println(true || false)
+fmt.Println(!true)
+var chr rune 
+chr = 'আ'
+fmt.Println(chr)
+fmt.Printf("%c", chr)*/
+}
